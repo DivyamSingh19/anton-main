9112041f95ac437396c129e1789357ed9204602a
Divyam Singh <divyamsingh1918@gmail.com>
2026-03-13 14:10:05 +0530

    base-engine-setup

--- engine/index.ts
+++ engine/index.ts
@@ -1,6 +1,16 @@
 //main engine to handle kill switch executions and timelocks
 import express from "express"
 import dotenv from "dotenv"
+import connectDB from "./mongo/connection"
 const app = express()
 dotenv.config()
 const port = process.env.PORT
+
+connectDB()
+
+
+
+
+app.listen(port,()=>{
+    console.log(`Kaizen Engine running on port ${port}`)
+})
--- engine/mongo/connection.ts
+++ engine/mongo/connection.ts
@@ -1,11 +1,10 @@
 import mongoose from "mongoose";
-const dbUri = process.env.MONGODB_URI
  
 const connectDB = async () => {
     mongoose.connection.on('connected',()=>{
         console.log("DB connected")
     })
-    await mongoose.connect(`${dbUri as string}/kaizen`)
+    await mongoose.connect(`${process.env.MONGODB_URI as string}/kaizen`)
 }
 
 export default connectDB;
