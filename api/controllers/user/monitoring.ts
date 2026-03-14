b6a38d8c9547328616eb35a347f5af8c66b881b2
Divyam Singh <divyamsingh1918@gmail.com>
2026-03-14 17:54:30 +0530

    updates

--- api/controllers/user/monitoring.ts
+++ api/controllers/user/monitoring.ts
@@ -1,14 +1,40 @@
 import prisma from "../../db/db";
 import { Request,Response } from "express";
+import { HTTPStatus } from "../../utils/httpstatus";
 
 
 export class MonitoringController{
     start = async (req:Request,res:Response) => {
         try {
-            
+            const userId = req.userId
+            if(!userId){
+                return res.status(HTTPStatus.Unauthorized).json({
+                    success:false,
+                    message:"User not authorized"
+                })
+            }
+            const {projectId} = req.body
         } catch (error) {
-            
+            return res.status(HTTPStatus.InternalError).json({
+                success:false,
+                error:(error as Error).message
+            })
         }
     }
-    
+    view = async (req:Request,res:Response) => {
+        try {
+            const userId  = req.userId
+            if(!userId){
+                return res.status(HTTPStatus.Unauthorized).json({
+                    success:false,
+                    message:"User not authorized"
+                })
+            }
+        } catch (error) {
+            return res.status(HTTPStatus.InternalError).json({
+                success:false,
+                error:(error as Error).message
+            })
+        }
+    }    
 }
