e61df8a297029bc34c01ee80b063b1fe896c2bf5
Divyam Singh <divyamsingh1918@gmail.com>
2026-05-29 14:36:56 +0530

    updates

---
+++ repo/apps/server/src/modules/telemetry/telemetry.controller.ts
@@ -0,0 +1,18 @@
+import {
+    Controller,
+    Post,
+    Get,
+    Body,
+    Req,
+    Res,
+    UseGuards
+} from "@nestjs/common";
+
+
+@Controller("api/telemetry")
+export class TelemetryController{
+    constructor(
+
+    )
+}
+
